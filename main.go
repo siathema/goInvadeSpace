9e0e9b931c84dd80b40ab53635780af788dbd380
Matthias <nate.holaday@gmail.com>
2023-05-17 12:07:27 -0700

    Added cpu and memory types

--- main.go
+++ main.go
@@ -3,6 +3,26 @@ package main
 import (
     "fmt"
 )
+// Use for main memory and io memory
+type MemoryMap struct {
+    DBus *uint8
+    ABus *uint16
+    Write bool
+    RamSz, RomSz uint
+    Rom, Ram *[]uint8
+}
+
+
+type core8080 struct {
+    A, B, C, D, E, H, L uint8
+    SP, PC uint16
+    DBus *uint8
+    ABus *uint16
+    Irq, Write, Sync bool
+    cycles uint64
+}
+
+
 
 func main() {
     fmt.Println("Hello weeb!")
