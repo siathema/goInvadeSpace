c80b334b1fe96dc5f1b609db1513b78b93232d9c
Matthias <nate.holaday@gmail.com>
2023-05-21 15:48:05 -0700

    Bleargh

--- main.go
+++ main.go
@@ -2,14 +2,26 @@ package main
 
 import (
     "fmt"
+    "os"
 )
+
+func Kilobytes(kb uint) uint {
+    return kb * 1024
+}
 // Use for main memory and io memory
 type MemoryMap struct {
     DBus *uint8
     ABus *uint16
     Write bool
-    RamSz, RomSz uint
-    Rom, Ram *[]uint8
+    Rom, Ram []uint8
+}
+
+type MainMemory MemoryMap
+
+func (mem *MainMemory) Init(romData []uint8) {
+   mem.Rom = romData
+   mem.Ram = make([]uint8, Kilobytes(8)) 
+   fmt.Println(len(mem.Ram))
 }
 
 
@@ -26,4 +38,16 @@ type core8080 struct {
 
 func main() {
     fmt.Println("Hello weeb!")
+    romData, err := os.ReadFile("roms/invaders.rom")
+    if err != nil {
+        panic(err)
+    }
+
+
+    memory := new(MainMemory)
+    memory.Init(romData)
+    fmt.Println(memory.Rom)
+    fmt.Println(len(romData))
+    
+    fmt.Printf("Memory Initialized with %dK of rom and %dK of ram!\n", len(memory.Rom)/1024, len(memory.Ram)/1024)
 }
