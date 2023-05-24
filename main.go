dd8bb05933a650337c03054b8db2ecaa89a7df8c
Matthias <nate.holaday@gmail.com>
2023-05-24 14:14:19 -0700

    Adding cpu functions

--- main.go
+++ main.go
@@ -8,10 +8,14 @@ import (
 func Kilobytes(kb uint) uint {
     return kb * 1024
 }
+
+type Bus8 uint8
+type Bus16 uint16
+
 // Use for main memory and io memory
 type MemoryMap struct {
-    DBus *uint8
-    ABus *uint16
+    DBus *Bus8
+    ABus *Bus16
     Write bool
     Rom, Ram []uint8
 }
@@ -24,17 +28,48 @@ func (mem *MainMemory) Init(romData []uint8) {
    fmt.Println(len(mem.Ram))
 }
 
+func (mem *MainMemory) Read(addr uint16) uint8 {
+    return 0
+}
 
-type core8080 struct {
+type Core8080 struct {
     A, B, C, D, E, H, L uint8
     SP, PC uint16
-    DBus *uint8
-    ABus *uint16
+    DBus *Bus8
+    ABus *Bus16
     Irq, Write, Sync bool
     cycles uint64
 }
 
+func (core *Core8080) Init(dataBus *Bus8, addrBus *Bus16) {
+    core.A = 0
+    core.B = 0
+    core.C = 0
+    core.D = 0
+    core.E = 0
+    core.H = 0
+    core.L = 0
+    core.DBus = dataBus
+    core.ABus = addrBus
+    core.Irq = false
+    core.Write = false
+    core.Sync = false
+}
 
+func (core *Core8080) Run(mem *MainMemory) {
+    // Read opcode from memory
+    core.Write = false
+    opcode := make([]uint8, 3)
+    for i :=0; i < 3; i++ {
+        opcode[i] = mem.Read(core.PC + uint16(i))
+    }
+    core.ExecuteOpcode(opcode)
+
+}
+
+func (core *Core8080) ExecuteOpcode(opcode []uint8) {
+    return
+}
 
 func main() {
     fmt.Println("Hello weeb!")
